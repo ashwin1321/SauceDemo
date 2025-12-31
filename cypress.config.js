7dc5ecc313de2bb00b4858a5f7a7f07ad2987095
ashwin1321 <ashwinkhatiwada7@gmail.com>
2025-12-31 15:08:00 +0545

    chore: added mochawesome for report generation

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,20 @@
-// const { defineConfig } = require("cypress");
 import { defineConfig } from "cypress";
-module.exports = defineConfig({
+import mochawesome from "cypress-mochawesome-reporter/plugin";
+
+export default defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      mochawesome(on); // register plugin
+      return config;
+    },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
     },
   },
 });
