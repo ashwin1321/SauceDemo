46bebb925f30db0ec4e3ba8a61455acbcb022731
ashwin1321 <ashwinkhatiwada7@gmail.com>
2025-12-31 14:57:28 +0545

    refactor

--- cypress/e2e/uiDemo.cy.js
+++ cypress/e2e/uiDemo.cy.js
@@ -1,15 +1,18 @@
 import CartPage from "../pages/cartPage.js";
 import InventoryPage from "../pages/inventoryPage.js";
 import LoginPage from "../pages/LoginPage.js"
-import topTwoItems from "../fixtures/topTwoItems.json"
+import topTwoItemss from "../fixtures/topTwoItems.json"
 
 let login = new LoginPage();
 let inventory = new InventoryPage();
-let cart = new CartPage();
+let cart = new CartPage()
 
 describe("UI demo Add to Cart", () => {
 
     it("Login and add to cart", () => {
+
+
+        let topTwoItems = topTwoItemss.items;
 
         // Login to the page
         login.visitUrl();
@@ -24,7 +27,7 @@ describe("UI demo Add to Cart", () => {
         inventory.sortProduct("highLow");
 
         // checking the item dynamcally and addting them to cart
-        topTwoItems.items.forEach((item, index) => {
+        topTwoItems.forEach((item, index) => {
             cy.log("------ finding 1st and 2nd element after sorting and validating ----------")
             cy.get(inventory.itemPrice).eq(index).should("contain", item.price);
             cy.get(inventory.itemName).eq(index).should("have.text", item.name);
@@ -36,7 +39,7 @@ describe("UI demo Add to Cart", () => {
             cy.log("----- items added to cart ----")
         });
 
-        cy.get(inventory.carticon).should("have.text", topTwoItems.items.length);
+        cy.get(inventory.carticon).should("have.text", topTwoItems.length);
         cy.log("-------- Validated the add to cart item numbers --------")
 
         // clicking cart icon
@@ -46,16 +49,16 @@ describe("UI demo Add to Cart", () => {
 
         // checking if there are exactly two items in the cart list
         cart.getCartLength().then(length => {
-            expect(length).to.eq(topTwoItems.items.length);
+            expect(length).to.eq(topTwoItems.length);
             cy.log("------ Cart Length Validated ------")
         })
 
         cy.log("-------- Checking the Cart Items and check if it is same as we expect --------")
 
-        topTwoItems.items.forEach((item, index) => {
+        topTwoItems.forEach((item, index) => {
             cart.getItemName(index).then(name => {
                 // checking if the item is in any item list that was added earlier
-                const exists = topTwoItems.items.some(item => item.name === name);
+                const exists = topTwoItems.some(item => item.name === name);
                 expect(exists).to.be.true;
                 cy.log("------ added cat name verified " + name + " ------");
             });
